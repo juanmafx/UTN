29819ee4ca56520309a795f746f7649c5f906e05
Juan Manuel Ramallo <jramallo1989@gmail.com>
2018-04-24 13:43:57 +0000

    Update imprimevector.java

--- 111Mil/imprimevector.java
+++ 111Mil/imprimevector.java
@@ -1,21 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package vector5x5;
 
 import java.util.*;
 
-/**
- *
- * @author alumno
- */
 public class Vector5x5 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
 
         Random r = new Random();
@@ -24,17 +12,17 @@ public class Vector5x5 {
         m = new int[5][5];
         for (int i = 0; i < m.length; i++) {
             for (int j = 0; j < m.length; j++) {
-                m[i][j] = r.nextInt(5);
+                m[i][j] = r.nextInt(1000);
 
             }
 
         }
 
         for (int x = 0; x < m.length; x++) {
-            System.out.println("---------");
+            System.out.println("+-------+--------+---------+----------+");
 
             for (int y = 0; y < m[x].length; y++) {
-                System.out.print("|" + m[x][y]);
+                System.out.printf("| %4d   " , m[x][y]);
             }
             System.out.println("|");
         }
