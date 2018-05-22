c0190113716be8898eaf6447c12bb2de625b4d21
Juan Manuel Ramallo <jramallo1989@gmail.com>
2018-05-22 13:20:17 +0000

    Create AsensorPoo1.java

---
+++ AsensorPoo1.java
@@ -0,0 +1,37 @@
+
+/*
+Un asensor
+Bootonera piso y el abitaculo.
+Capcidad de 8
+
+Numerados del 0 al 20.
+
+No pasajeros =Reposo y no se muebe hasta que es llamado no tiene memoria.
+
+Si la capacidad es superada no pasajeros y no se muebe.
+
+
+Clase asensor Simule el coportamiento del asensor.
+El piso y la cantidad de personas alojadas.
+
+¿Que metodos deberian programarse?
+¿Que atributos harian falta para representar su estado?
+¿Que estadisticas podrian obtenerte ?
+
+
+package asensorpoo1;
+
+/**
+ *
+ * @author alumno
+ */
+public class AsensorPoo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
