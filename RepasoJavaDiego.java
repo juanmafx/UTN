d370e975818d6c1c6e39a6d421da59db3deb30b3
Juan Manuel Ramallo <jramallo1989@gmail.com>
2018-06-05 12:47:08 -0300

    Update RepasoJavaDiego.java

--- RepasoJavaDiego.java
+++ RepasoJavaDiego.java
@@ -14,10 +14,18 @@ Luego de cargar informar:
 -Por cada numero  buscado, informar
 -Si se enceçuentra o no :
 En caso de encontrarlo, la posicion donde se encuentra
+
+
+Carga datos desd un file 
+scanner sc new=scanner ("numeros.txt")
+Scanner archivo = new Scanner("numeros.txt");
+Scanner teclado = new Scanner(System.in);
  */
 package repasojavadiego;
 
 import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 /**
  *
@@ -27,17 +35,19 @@ public class RepasoJavaDiego {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.FileNotFoundException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
         Scanner sc = new Scanner(System.in);
+        Scanner archivo = new Scanner(new File("numeros.txt"));
         float cont = 0;
         float cont_pos = 0;
         boolean esta15 = false;
         float acu_pares = 0;
         float pro_pares = 0;
         float may = 0;
-        float acumay = 0;
+        float contmay = 0;
         float buscado;
         float pos;
         float valido = 0;
@@ -48,11 +58,10 @@ public class RepasoJavaDiego {
 
         for (int i = 0; i < vec.length; i++) {
             cont++;
-            vec[i] = sc.nextInt();
-
+            vec[i] = archivo.nextInt();
+            System.out.println(vec[i]);
             if (vec[i] == 0) {
                 break;
-
             }
 
         }
@@ -62,35 +71,33 @@ public class RepasoJavaDiego {
             if (vec[j] > 0) {
                 cont_pos++;
             }
-		}
+        }
 
-		for (int j = 0; j < cont; j++) {
+        for (int j = 0; j < cont; j++) {
             if (vec[j] == 15) {
                 esta15 = true;
 
             }
-            }
-for (int j = 0; j < cont; j++) {
+        }
+        for (int j = 0; j < cont; j++) {
             if (vec[j] % 2 == 0) {
                 acu_pares = acu_pares + vec[j];
             }
+        }
+        may = vec[0];
+        for (int j = 0; j < cont; j++) {
+            if (vec[j] == may) {
+                contmay++;
             }
-for (int j = 0; j < cont; j++) {
+
             if (may < vec[j]) {
                 may = vec[j];
+                contmay = 1;
 
             }
-            }
-for (int j = 0; j < cont; j++) {
-            if (vec[j] == may) {
-                acumay++;
+        }
 
-            }
-            }
-
-        
-
-        pro_pares = acu_pares / cont ;
+        pro_pares = acu_pares / cont;
 
         // Muestra el Vector
         for (int x = 0; x < cont; x++) {
@@ -98,6 +105,7 @@ for (int j = 0; j < cont; j++) {
             System.out.printf("| %4d   ", vec[x]);
 
         }
+        System.out.printf("|");
 
         //INFORMA
         System.out.println(" ");
@@ -106,7 +114,7 @@ for (int j = 0; j < cont; j++) {
         System.out.println("Cantidad de numeros  POSITIVOS  ingresados : " + cont_pos);
         System.out.println("Alguna vez se ingreso el 15 ? = " + esta15);
         System.out.println("Promedio de los PARES ingresados = " + pro_pares);
-        System.out.println("El mayor es  = " + may + "Y se ingreso = " + (acumay));
+        System.out.println("El mayor es  = " + may + "Y se ingreso = " + (contmay));
 
         System.out.println("Ingrese numero que desea buscar");
         buscado = sc.nextInt();
