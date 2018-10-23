9aad68110d2429de8960e7f4857d626d8077b522
Juan Manuel Ramallo <jramallo1989@gmail.com>
2018-10-23 14:27:42 -0300

    Create clase.cs

---
+++ clase.cs
@@ -0,0 +1,48 @@
+        int codigo;
+
+        public int pCodigo
+        {
+            get { return codigo; }
+            set { codigo = value; }
+        }
+
+        string detalle;
+
+        public string pDetalle
+        {
+            get { return detalle; }
+            set { detalle = value; }
+        }
+        double precio;
+
+        public double pPrecio
+        {
+            get { return precio; }
+            set { precio = value; }
+        }
+        int tipo;
+
+        public int pTipo
+        {
+            get { return tipo; }
+            set { tipo = value; }
+        }
+        int marca;
+
+        public int pMarca
+        {
+            get { return marca; }
+            set { marca = value; }
+        }
+        DateTime fecha;
+
+        public DateTime pFecha
+        {
+            get { return fecha; }
+            set { fecha = value; }
+        }
+
+        override public string ToString()
+        {
+            return codigo + " - " + detalle;
+        }
